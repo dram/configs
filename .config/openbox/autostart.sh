e065183a1d508417ec8d4b91a386e3ccd8a8e188
Xin Wang <dram.wang@gmail.com>
2011-05-31 19:58:00 +0800

    Move all code in .xinitrc to autostart.sh.

--- .config/openbox/autostart.sh
+++ .config/openbox/autostart.sh
@@ -1,9 +1,34 @@
 # This shell script is run before Openbox launches.
 # Environment variables set here are passed to the Openbox session.
 
-source $HOME/.fehbg
+export LANG=en_US.utf8
+
+echo -ne '\033%G'
+
+export PATH=$HOME/bin:/sbin:/usr/sbin:$PATH
+
+export GTK_IM_MODULE=uim
+export XMODIFIERS=@im=uim
+export QT_IM_MODULE=uim
+
+install -dm700 /dev/shm/firefox-cache
+mkdir -p -m777 /dev/shm/firefox-cache/Cache
+
+vim-mpg123.py -d
+
+wm-assist.py -D >>$HOME/.wm-assist.log 2>&1
+
+xchainkeys -d >$HOME/.xchainkeys.log 2>&1 &
+
+#uim-toolbar-gtk-systray &
+
+/usr/sbin/VBoxClient --clipboard &
+
+( source $HOME/.fehbg )
 
 tint2 &
+
+xinput set-int-prop "ImExPS/2 Generic Explorer Mouse" "Evdev Middle Button Emulation" 8 1
 
 # D-bus
 if which dbus-launch >/dev/null && test -z "$DBUS_SESSION_BUS_ADDRESS"; then
