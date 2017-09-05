c5692dbf50249e3f369dd350300b946dd31ddbc8
Xin Wang <dram.wang@gmail.com>
2017-09-05 20:17:00 +0800

    Require a final newline

--- emacs/dotemacs.el
+++ emacs/dotemacs.el
@@ -71,8 +71,6 @@
 	(shell-command (concat "lynx -dump -nolist -width=78 " url))
 	(pop-to-buffer "*Shell Command Output*")))
 
-(setq mode-require-final-newline nil)
-
 ;; appearance
 
 (setq inhibit-startup-message t)
