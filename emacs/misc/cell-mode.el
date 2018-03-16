bc8f08ec82f9aa2ff3963d4d11c6a32f6e7548e1
Xin Wang <dram.wang@gmail.com>
2018-03-16 18:18:15 +0800

    Highlight type using cell-font-lock-extra-types

--- emacs/misc/cell-mode.el
+++ emacs/misc/cell-mode.el
@@ -1,48 +1,22 @@
-(defvar cell-mode-type-signature-regex
-  ;; Foo
-  "\\([[:upper:]]\\w*\\)")
-(defvar cell-mode-set-type-signature-regex
-  ;; [Foo]
-  "\\[\\([[:upper:]]\\w*\\)\\*?\\]")
-(defvar cell-mode-generic-type-signature-regex
-  ;; Foo[Bar]
-  "\\([[:upper:]]\\w*\\)\\[\\([[:upper:]]\\w*\\)\\*?\\]")
-(defvar cell-mode-map-type-signature-regex
-  ;; [Foo -> Bar]
-  "\\[\\([[:upper:]]\\w*\\)\\*?\\s-*->\\s-*\\([[:upper:]]\\w*\\)\\*?\\]")
+(require 'cc-mode)
+
+(c-add-language 'cell-mode 'c-mode)
+
+(setq cell-font-lock-extra-types '("[[:upper:]]\\sw*"))
 
 (defvar cell-mode-font-lock-keywords
   `(("\\<\\(after\\|and\\|apply\\|assert\\|assume\\|break\\|class\\|delete\\|elif\\|elapsed\\|else\\|every\\|fail\\|false\\|for\\|if\\|implicit\\|insert\\|let\\|loop\\|match\\|not\\|or\\|print\\|protocol\\|reactive\\|read\\|return\\|sans\\|schema\\|set\\|since\\|then\\|true\\|type\\|typevar\\|undefined\\|update\\|upon\\|using\\|volatile\\|when\\|while\\|write\\)\\>" . font-lock-keyword-face)
-    (,(concat "::\\s-*" cell-mode-type-signature-regex)
-     (1 font-lock-type-face))
-    (,cell-mode-set-type-signature-regex
-     (1 font-lock-type-face))
-    (,cell-mode-generic-type-signature-regex
-     (1 font-lock-type-face)
-     (2 font-lock-type-face))
-    (,cell-mode-map-type-signature-regex
-     (1 font-lock-type-face)
-     (2 font-lock-type-face))
-    (,(concat "\\s-*" cell-mode-type-signature-regex "\\s-*\\(\\w+\\)")
-     (1 font-lock-type-face)
-     (2 font-lock-function-name-face))
-    (,(concat "\\s-*" cell-mode-set-type-signature-regex "\\s-*\\(\\w+\\)")
-     (1 font-lock-type-face)
-     (2 font-lock-function-name-face))
-    (,(concat "\\s-*" cell-mode-generic-type-signature-regex "\\s-*\\(\\w+\\)")
-     (1 font-lock-type-face)
-     (2 font-lock-type-face)
-     (3 font-lock-function-name-face))
-    (,(concat "\\s-*" cell-mode-map-type-signature-regex "\\s-*\\(\\w+\\)")
-     (1 font-lock-type-face)
-     (2 font-lock-type-face)
-     (3 font-lock-function-name-face))))
+    ("\\<\\(abs\\|all\\|any\\|append\\|bit\\|bwand\\|bwor\\|chars\\|difference\\|disjoint\\|drop\\|apply\\|error\\|failed\\|intermix\\|intersection\\|is_digit\\|is_lower\\|is_space\\|is_upper\\|isort\\|join\\|just\\|length\\|lower\\|max\\|maybe\\|merge\\|min\\|mod\\|nat\\|none\\|nothing\\|only\\|result\\|reverse\\|slice\\|sqrt\\|string\\|subset\\|substr\\|succeeded\\|sum\\|take\\|union\\|untag\\|unzip\\|unzip3\\|upper\\|value\\|value_unsafe\\|values\\|xor\\|zip\\)\\>" . font-lock-builtin-face)))
 
 (define-derived-mode cell-mode c-mode "Cell"
   "Major mode for editing Cell code."
   (setq-local syntax-propertize-function
               (syntax-propertize-rules ("\\(#\\)\\(#\\)"
                                         (1 "< 1b") (2 "< 2b"))))
+
+  (c-initialize-cc-mode t)
+  (c-init-language-vars cell-mode)
+
   (font-lock-add-keywords nil cell-mode-font-lock-keywords))
 
 (add-to-list 'auto-mode-alist '("\\.cell$" . cell-mode))
