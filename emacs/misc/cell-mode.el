0fc854f13d68c691f819510cc525614fca516003
Xin Wang <dram.wang@gmail.com>
2018-03-16 12:36:54 +0800

    Add more font-lock config to cell-mode

--- emacs/misc/cell-mode.el
+++ emacs/misc/cell-mode.el
@@ -1,6 +1,33 @@
+(defvar cell-mode-set-type-signature-regex
+  ;; [Foo]
+  "\\[\\([[:upper:]]\\w*\\)\\*?\\]")
+(defvar cell-mode-generic-type-signature-regex
+  ;; Foo[Bar]
+  "\\([[:upper:]]\\w*\\)\\[\\([[:upper:]]\\w*\\)\\*?\\]")
+(defvar cell-mode-map-type-signature-regex
+  ;; [Foo -> Bar]
+  "\\[\\([[:upper:]]\\w*\\)\\*?\\s-*->\\s-*\\([[:upper:]]\\w*\\)\\*?\\]")
+
 (defvar cell-mode-font-lock-keywords
-  '(("\\<\\(after\\|and\\|apply\\|assert\\|assume\\|break\\|class\\|delete\\|elif\\|elapsed\\|else\\|every\\|fail\\|false\\|for\\|if\\|implicit\\|insert\\|let\\|loop\\|match\\|not\\|or\\|print\\|protocol\\|reactive\\|read\\|return\\|sans\\|schema\\|set\\|since\\|then\\|true\\|type\\|typevar\\|undefined\\|update\\|upon\\|using\\|volatile\\|when\\|while\\|write\\)\\>" . font-lock-keyword-face)
-    ("\\(\\w+\\)\\[\\(\\w+\\)\\*?\\]\\s-+\\(\\w+\\)"
+  `(("\\<\\(after\\|and\\|apply\\|assert\\|assume\\|break\\|class\\|delete\\|elif\\|elapsed\\|else\\|every\\|fail\\|false\\|for\\|if\\|implicit\\|insert\\|let\\|loop\\|match\\|not\\|or\\|print\\|protocol\\|reactive\\|read\\|return\\|sans\\|schema\\|set\\|since\\|then\\|true\\|type\\|typevar\\|undefined\\|update\\|upon\\|using\\|volatile\\|when\\|while\\|write\\)\\>" . font-lock-keyword-face)
+    (":: \\([[:upper:]]\\w*\\)"
+     (1 font-lock-type-face))
+    (,cell-mode-set-type-signature-regex
+     (1 font-lock-type-face))
+    (,cell-mode-generic-type-signature-regex
+     (1 font-lock-type-face)
+     (2 font-lock-type-face))
+    (,cell-mode-map-type-signature-regex
+     (1 font-lock-type-face)
+     (2 font-lock-type-face))
+    (,(concat cell-mode-set-type-signature-regex "\\s-*\\(\\w+\\)(")
+     (1 font-lock-type-face)
+     (2 font-lock-function-name-face))
+    (,(concat cell-mode-generic-type-signature-regex "\\s-*\\(\\w+\\)(")
+     (1 font-lock-type-face)
+     (2 font-lock-type-face)
+     (3 font-lock-function-name-face))
+    (,(concat cell-mode-map-type-signature-regex "\\s-*\\(\\w+\\)(")
      (1 font-lock-type-face)
      (2 font-lock-type-face)
      (3 font-lock-function-name-face))))
